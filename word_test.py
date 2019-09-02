f85581f8368a5e4c3dbe1b600dcff801b6ac90a1
KyeongSeob Sim <ksub0912@gmail.com>
2019-09-02 18:04:07 +0900

    Added practice mode

--- word_test.py
+++ word_test.py
@@ -23,15 +23,24 @@ def get_word_list(file_path):
 def get_word_files(directory_path):
     return glob(directory_path + "/*.word")
 
-def word_test(word_list_dic, reverse):
+def practice():
+    while True:
+        if input() == "n":
+            break
+
+def word_test(word_list_dic, reverse, practice_mode):
     words = list(word_list_dic.keys())
     shuffle(words)
 
     for i, word in enumerate(words):
         print ("===========")
         print ("%s. %s" % (i+1, word_list_dic[word] if reverse else word))
-        input()
-        print (word if reverse else word_list_dic[word])
+        user_input = input()
+        answer = word if reverse else word_list_dic[word]
+        print (answer)
+        if practice_mode and user_input != answer:
+            practice()
+
         print ("===========")
         print ("\n")
 
@@ -40,6 +49,7 @@ def main():
     parser.add_option("-f", "--file", dest="file_path", help="word file location")
     parser.add_option("-r", "--reverse", dest="reverse", action="store_true", help="word & meaning reverse")
     parser.add_option("-d", "--directory", dest="directory_path", help="word file directy")
+    parser.add_option("-p", "--practice", dest="practice_mode", action="store_true", help="set the practice mode")
 
     (options, _) = parser.parse_args()
 
@@ -47,13 +57,13 @@ def main():
         parser.print_help()
         exit(1)
     elif options.file_path:
-        word_test(get_word_list(options.file_path), options.reverse)
+        word_test(get_word_list(options.file_path), options.reverse, options.practice_mode)
     elif options.directory_path:
         word_list = {}
         for word_file in get_word_files(options.directory_path):
             word_list.update(get_word_list(word_file))
 
-        word_test(word_list, options.reverse)
+        word_test(word_list, options.reverse, options.practice_mode)
     else:
         parser.print_help()
         exit(1)
